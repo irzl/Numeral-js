440985318db1a439d351ee5c9e9e973cb47ade8e
Irfan Anshari <hello@irzl.dev>
2020-10-07 10:04:12 +0700

    Create id-sr.js
    Short for Locale ID

---
+++ locales/id-sr.js
@@ -0,0 +1,32 @@
+// numeral.js locale configuration
+// locale : Indonesia / Indonesian (id)
+// author : Irfan Anshari : https://github.com/irzl
+
+(function (global, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define(['../numeral'], factory);
+  } else if (typeof module === 'object' && module.exports) {
+    factory(require('../numeral'));
+  } else {
+    factory(global.numeral);
+  }
+}(this, function (numeral) {
+  numeral.register("locale", "id", {
+    delimiters: {
+      thousands: ".",
+      decimal: ","
+    },
+    abbreviations: {
+      thousand: "Rb",
+      million: "Jt",
+      billion: "M",
+      trillion: "T"
+    },
+    ordinal: function (number) {
+      return '.';
+    },
+    currency: {
+      symbol: "Rp"
+    }
+  })
+}));
